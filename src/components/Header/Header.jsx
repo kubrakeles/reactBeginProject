6e066794e846a90ce49a1cc2d2eec61da3810632
kübra keleş <kubra.keles@outlook.com>
2024-03-27 16:22:35 +0300

    Email js updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -42,7 +42,7 @@ if(document.documentElement.clientWidth <= 800)
             Sıkça Sorulan Sorular
           </Link>
           
-          <Link to="/EmailPage">
+          <Link to="/">
             Teklif Al
           </Link>
           <button className='button'>
